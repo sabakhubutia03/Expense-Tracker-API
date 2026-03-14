e58317c0a33e389007b878b7fcc2f6fc0ee594d5
sabakhubutia03 <saba.khubutia03@geolab.edu.ge>
2026-03-14 16:20:50 +0400

    docs: cleanup repository and apply gitignore correctly

--- ExpenseTracker.Application/obj/Debug/net9.0/ExpenseTracker.Application.AssemblyInfo.cs
+++ ExpenseTracker.Application/obj/Debug/net9.0/ExpenseTracker.Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ExpenseTracker.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bec516ac8bd2dd6f71a8ad174fdc8a0c6e507024")]
 [assembly: System.Reflection.AssemblyProductAttribute("ExpenseTracker.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ExpenseTracker.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
