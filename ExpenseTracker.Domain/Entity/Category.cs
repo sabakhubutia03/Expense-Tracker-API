69ef832f4bbc1850f93f7ad8086a415e0f6cc5bf
sabakhubutia03 <saba.khubutia03@geolab.edu.ge>
2026-03-14 14:36:05 +0400

    Initial commit - created ExpenseTracker solution with four projects (Domain, Application, Infrastructure, API). Domain entities implemented.

---
+++ ExpenseTracker.Domain/Entity/Category.cs
@@ -0,0 +1,9 @@
+﻿namespace ExpenseTracker.Domain.Entity;
+
+public class Category
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public ICollection<Expense> Expenses { get; set; } = new List<Expense>();
+    
+}
---
+++ ExpenseTracker.Domain/Entity/Expense.cs
@@ -0,0 +1,16 @@
+﻿namespace ExpenseTracker.Domain.Entity;
+
+public class Expense
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public double Amount { get; set; }
+    public DateTime Date { get; set; } = DateTime.Now;
+    
+    
+    public int UserId { get; set; }
+    public User User { get; set; }
+    
+    public int CategoryId { get; set; }
+    public Category Category { get; set; }
+}
